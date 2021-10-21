de703893096b2074ef8a3dcedd5471904e234674
Anton Babenko <antonwarbeck123@gmail.com>
2021-10-21 15:02:35 +0300

    Speeded up blur

--- blur.py
+++ blur.py
@@ -13,30 +13,20 @@ def get_gaussian_kernel(x_size, y_size, sigma):
     return kernel
 
 
-def patch_multiplication(image, weight, x, y):
-    result = 0
-    for i in range(weight.shape[0]):
-        for j in range(weight.shape[1]):
-            image_i = x + i - weight.shape[0] // 2
-            image_j = y + j - weight.shape[1] // 2
-
-            if image_i < 0 or image_j < 0 or image_i >= image.shape[0] or \
-                    image_j >= image.shape[1]:
-                continue
-            result = result + image[image_i, image_j] * weight[i, j]
-    return int(result)
-
-
 def conv2d(image, weight):
     output = np.zeros(image.shape)
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            output[i, j] = patch_multiplication(image, weight, i, j)
+    x_size = weight.shape[0] // 2
+    y_size = weight.shape[1] // 2
+    image = np.pad(image, ((y_size,), (x_size,)), mode="constant")
+    for i in range(x_size, image.shape[0] - x_size):
+        for j in range(y_size, image.shape[1] - y_size):
+            patch = image[i-x_size:i+x_size+1, j-y_size:j+y_size+1]
+            output[i - x_size, j - y_size] = np.sum(patch * weight)
     return output
 
 
 def gaussian_filter(image, sigma, truncate=3.):
-    width = int(sigma * truncate + 0.5) + 1
+    width = 2 * int(sigma * truncate + 0.5) + 1
     kernel = get_gaussian_kernel(width, width, sigma)
     output = conv2d(image, kernel)
     return output
